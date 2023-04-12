1ff7151a6081f2132e253c2e97bf67fa0757cda6
Ahmed Ibrahim Elsabaa <ahmed101ibrahiem>
2023-04-12 05:13:27 +0200

    refactor init state & nav home

--- lib/features/splash/presentation/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/widgets/splash_view_body.dart
@@ -1,6 +1,9 @@
+import 'package:book_reader/constants.dart';
 import 'package:book_reader/core/utils/assets_data.dart';
 import 'package:book_reader/features/splash/presentation/widgets/splash_text_animation.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../../home/presentation/views/home_view.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({Key? key}) : super(key: key);
@@ -17,17 +20,15 @@ class _SplashViewBodyState extends State<SplashViewBody>
 
   @override
   void initState() {
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 1),
-    );
-    slidingAnimation = Tween<Offset>(begin: const Offset(-0.5,8), end: Offset.zero)
-        .animate(animationController);
-    imgAnimation = Tween<Offset>(begin: const Offset(0.2,-2), end: Offset.zero)
-        .animate(animationController);
-    animationController.forward();
-
+    initSlidingAnimation();
+    navToHome();
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
   }
 
   @override
@@ -38,10 +39,10 @@ class _SplashViewBodyState extends State<SplashViewBody>
       children: [
         AnimatedBuilder(
           animation: imgAnimation,
-          builder: (context, _) =>
-           SlideTransition(
+          builder: (context, _) => SlideTransition(
             position: imgAnimation,
-              child: Image.asset(AssetsData.logoImg)),
+            child: Image.asset(AssetsData.logoImg),
+          ),
         ),
         const SizedBox(
           height: 8.0,
@@ -49,5 +50,25 @@ class _SplashViewBodyState extends State<SplashViewBody>
         SplashTextAnimation(slidingAnimation: slidingAnimation),
       ],
     );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(-0.5, 8), end: Offset.zero)
+            .animate(animationController);
+    imgAnimation = Tween<Offset>(begin: const Offset(0.2, -2), end: Offset.zero)
+        .animate(animationController);
+    animationController.forward();
+  }
+
+  void navToHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(() => const HomeView(),
+          transition: Transition.fade, duration: kTranstionDuration);
+    });
   }
 }
